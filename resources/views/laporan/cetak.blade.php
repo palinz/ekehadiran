84d6b5cc9ffbf1404d8908b59bd2bed306f82754
palinz <82084953+palinz@users.noreply.github.com>
2021-04-30 14:18:54 +0800

    Update cetak.blade.php

--- resources/views/laporan/cetak.blade.php
+++ resources/views/laporan/cetak.blade.php
@@ -7,28 +7,109 @@
 </head>
 
 <body bgcolor="#F6E9DC" style="width:100%">
-		<table style="width:100%">
-			<h6 class="text-center"> MINIT CURAI</h6>
-            <h6 class="text-center"> MESYUARAT LUAR / BENGKEL / KURSUS / PROGRAM </h6>
-            <h6 class="text-center">  JABATAN PENDIDIKAN NEGERI MELAKA</h6>
-			<tr>
-				<td>Mesyuarat luar / Bengkel / Kursus / Program : </td>
-			</tr>
-			<tr>
-				<td>Anjuran: </td>
-			</tr>
-			<tr>
-				<td>Tarikh / Masa / Tempat: </td>
-			</tr>
-			<tr>
-				<td>Pegawai Yang Terlibat: </td>
-			</tr>
-			<tr>
-				<td>Isu / Isu Penting Mesyuaray luar / Bengkel / Kursus / Program: </td>
-			</tr>
-			<tr>
-				<td>Nyatakan Tindakan Yang Mesti / Perlu Diambil: </td>
-			</tr>
-		</table>
+		<table width="747" border="0" align="center">
+	<tr>
+    <td width="116" align="center"><img src="LogoJPNMelaka.png"></td>
+	</tr>
+</table>
+<table width="747" border="0" align="center">
+    <tr>
+    <td colspan="6" align="center" class="style6">	
+    <p><strong>MINIT CURAI </strong></p></td>
+	</tr>
+    <tr>
+    <td colspan="6" align="center" class="style6">	
+    <p><strong>MESYUARAT LUAR / BENGKEL / KURSUS / PROGRAM </strong></p></td>
+	</tr>
+    <tr>
+    <td colspan="6" align="center" class="style6">	
+    <p><strong>JABATAN PENDIDIKAN NEGERI MELAKA </strong></p></td>
+	</tr>
+  	<tr>	
+  	</tr>  
+	<tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>			
+	<tr>
+	  <td colspan="6" align="left"  valign="top" class="style6"><p>1. &nbsp;&nbsp;&nbsp;
+      Mesyuarat luar / Bengkel / Kursus / Program:</p>      </td>
+    </tr>
+	<tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+	<tr>
+	  <td colspan="6" align="left"  valign="top" class="style6"><p>2. &nbsp;&nbsp;&nbsp;
+      Anjuran:</p>      </td>
+    </tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr>
+	  <td colspan="6" align="left"  valign="top" class="style6"><p>3. &nbsp;&nbsp;&nbsp;
+      Tarikh / Masa / Tempat:</p>      </td>
+    </tr>
+	<tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+	<tr>
+	  <td colspan="6" align="left"  valign="top" class="style6"><p>4. &nbsp;&nbsp;&nbsp;
+      Pegawai Yang Terlibat:</p>      </td>
+    </tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+	<tr>
+	  <td colspan="6" align="left"  valign="top" class="style6"><p>5. &nbsp;&nbsp;&nbsp;
+      Isu / Isu Penting Mesyuarat luar / Bengkel / Kursus / Program:</p>      </td>
+    </tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+	<tr>
+	  <td colspan="6" align="left"  valign="top" class="style6"><p>6. &nbsp;&nbsp;&nbsp;
+      Nyatakan Tindakan Yang Mesti / Perlu Diambil:</p>      </td>
+    </tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>			
+  	<tr>
+    	<td width="36" align="left" class="style6">&nbsp;</td>
+   	  <td colspan="5" align="left" class="style6"><br></td>
+  	</tr>
+</table>
+<table width="747" border="0" align="center">
+                                <tr>
+                                    <td colspan = '6' height='0'>
+                                </tr>
+                                <tr>
+                                    <td colspan = '6' height='0'>Tanda Tangan Pegawai</td>
+                                    <td colspan = '6' height='0'>Tarikh</td>
+                                </tr>
+                                    <td colspan = '6' height='0'><br><br>
+                                        ................................................
+                                        <br><br>
+                                    </td>
+                                    <td colspan = '6' height='0'><br><br>
+                                        ................................................
+                                        <br><br>
+                                    </td>
+    <tr>
+	    <td colspan="6" align="left"  valign="top" class="style6">Arahan / Cadangan bagi Tindakan / Keputusan oleh Pengarah/Timbalan Pengarah:</td>
+   	</tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>	
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+       <tr>
+                                    <td colspan = '6' height='0'>
+                                </tr>
+                                <tr>
+                                    <td colspan = '6' height='0'>Tanda Tangan Pengarah / Timbalan Pengarah</td>
+                                    <td colspan = '6' height='0'>Tarikh</td>
+                                </tr>
+                                    <td colspan = '6' height='0'><br><br>
+                                        .................................................
+                                        <br><br><br>
+                                    </td>
+                                    <td colspan = '6' height='0'><br><br>
+                                        .................................................
+                                        <br><br><br>
+                                    </td>
+                                </tr>       
+</table>
+
 </body>
 </html>
