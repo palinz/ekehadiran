ea5a3c8e174cc638f2014b30855d76c9c4f366df
palinz <82084953+palinz@users.noreply.github.com>
2021-05-03 11:44:44 +0800

    Update cetak.blade.php

--- resources/views/laporan/cetak.blade.php
+++ resources/views/laporan/cetak.blade.php
@@ -6,6 +6,62 @@
   <style type="text/css" ></style>
 </head>
 <body>
-HAI
+<table width="747" border="0" align="center">
+    <tr>
+    <td colspan="6" align="center" class="style6">	
+    <p><strong>MINIT CURAI </strong></p></td>
+	</tr>
+    <tr>
+    <td colspan="6" align="center" class="style6">	
+    <p><strong>MESYUARAT LUAR / BENGKEL / KURSUS / PROGRAM </strong></p></td>
+	</tr>
+    <tr>
+    <td colspan="6" align="center" class="style6">	
+    <p><strong>JABATAN PENDIDIKAN NEGERI MELAKA </strong></p></td>
+	</tr>
+  	<tr>	
+  	</tr>  
+	<tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>			
+	<tr>
+	  <td colspan="6" align="left"  valign="top" class="style6"><p>1. &nbsp;&nbsp;&nbsp;
+      Mesyuarat luar / Bengkel / Kursus / Program:</p>      </td>
+    </tr>
+	<tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+	<tr>
+	  <td colspan="6" align="left"  valign="top" class="style6"><p>2. &nbsp;&nbsp;&nbsp;
+      Anjuran:</p>      </td>
+    </tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr>
+	  <td colspan="6" align="left"  valign="top" class="style6"><p>3. &nbsp;&nbsp;&nbsp;
+      Tarikh / Masa / Tempat:</p>      </td>
+    </tr>
+	<tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+	<tr>
+	  <td colspan="6" align="left"  valign="top" class="style6"><p>4. &nbsp;&nbsp;&nbsp;
+      Pegawai Yang Terlibat:</p>      </td>
+    </tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+	<tr>
+	  <td colspan="6" align="left"  valign="top" class="style6"><p>5. &nbsp;&nbsp;&nbsp;
+      Isu / Isu Penting Mesyuarat luar / Bengkel / Kursus / Program:</p>      </td>
+    </tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+	<tr>
+	  <td colspan="6" align="left"  valign="top" class="style6"><p>6. &nbsp;&nbsp;&nbsp;
+      Nyatakan Tindakan Yang Mesti / Perlu Diambil:</p>      </td>
+    </tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>
+    <tr><td colspan="6" align="left" class="style6"><p>&nbsp;</p></td></tr>			
+  	<tr>
+    	<td width="36" align="left" class="style6">&nbsp;</td>
+   	  <td colspan="5" align="left" class="style6"><br></td>
+  	</tr>
+</table>
 </body>
 </html>
