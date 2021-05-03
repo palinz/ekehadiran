3e91638d72ac4f7928aaf97d6c598eaaa2a2ef86
palinz <82084953+palinz@users.noreply.github.com>
2021-05-03 11:57:16 +0800

    Update cetak.blade.php

--- resources/views/laporan/cetak.blade.php
+++ resources/views/laporan/cetak.blade.php
@@ -6,42 +6,6 @@
   <style type="text/css" ></style>
 </head>
 <body>
-<table width="747" border="0" align="center">
-    <tr>
-    <td colspan="6" align="center" class="style6">	
-    <strong>MINIT CURAI </strong></td>
-	</tr>
-    <tr>
-    <td colspan="6" align="center" class="style6">	
-    <strong>MESYUARAT LUAR / BENGKEL / KURSUS / PROGRAM </strong></td>
-	</tr>
-    <tr>
-    <td colspan="6" align="center" class="style6">	
-    <strong>JABATAN PENDIDIKAN NEGERI MELAKA </strong></td>
-	</tr>	
-	  <td colspan="6" align="left"  valign="top" class="style6"><p>1. &nbsp;&nbsp;&nbsp;
-      Mesyuarat luar / Bengkel / Kursus / Program:</p>      </td>
-    </tr>
-	<tr>
-	  <td colspan="6" align="left"  valign="top" class="style6"><p>2. &nbsp;&nbsp;&nbsp;
-      Anjuran:</p>      </td>
-    </tr>
-    <tr>
-	  <td colspan="6" align="left"  valign="top" class="style6"><p>3. &nbsp;&nbsp;&nbsp;
-      Tarikh / Masa / Tempat:</p>      </td>
-    </tr>
-	<tr>
-	  <td colspan="6" align="left"  valign="top" class="style6"><p>4. &nbsp;&nbsp;&nbsp;
-      Pegawai Yang Terlibat:</p>      </td>
-    </tr>
-	<tr>
-	  <td colspan="6" align="left"  valign="top" class="style6"><p>5. &nbsp;&nbsp;&nbsp;
-      Isu / Isu Penting Mesyuarat luar / Bengkel / Kursus / Program:</p>      </td>
-    </tr>
-	<tr>
-	  <td colspan="6" align="left"  valign="top" class="style6"><p>6. &nbsp;&nbsp;&nbsp;
-      Nyatakan Tindakan Yang Mesti / Perlu Diambil:</p>      </td>
-    </tr>			
-</table>
+hye
 </body>
 </html>
