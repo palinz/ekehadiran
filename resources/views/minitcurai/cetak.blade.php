93d67af1e579af5c663055a35a5286df5c034635
palinz <82084953+palinz@users.noreply.github.com>
2021-07-02 11:56:32 +0800

    create new cetak.blade.php

---
+++ resources/views/minitcurai/cetak.blade.php
@@ -0,0 +1,54 @@
+@inject('MinitCurai', 'App\MinitCurai')
+
+<div class="table-responesive" id="cetak-minit-curai-app">
+    <form id="frm-cetak-minit-curai">
+        @method('GET')
+        <head>
+		  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		  <title>MINIT CURAI</title>
+		  <style type="text/css" ></style>
+		</head>
+		<table class="table table-bordered">
+            <tbody>
+                <tr>
+                    <td class="col-md-3"><b>Aktiviti</b></td>
+                    <td><input class="form-control" type="text" name="txtAktiviti" placeholder="Tajuk Aktiviti" value="{{ $minitCurai->tajuk }}" required></td>
+                </tr>
+                <tr>
+                    <td><b>Anjuran</b></td>
+                    <td><input class="form-control" type="text" name="txtAnjuran" placeholder="Anjuran" value="{{ $minitCurai->anjuran }}" required></td>
+                </tr>
+                <tr>
+                    <td><b>Tarikh dan Masa</b></td>
+                    <td><input id="txtTarikhCurai" class="form-control" type="date" name="txtTarikh" placeholder="Tarikh" value="{{ $minitCurai->tarikh->format('Y-m-d') }}" required></td>
+                </tr>
+                <tr>
+                    <td><b>Tempat</b></td>
+                    <td><input class="form-control" type="text" name="txtTempat" placeholder="Tempat" value="{{ $minitCurai->lokasi }}" required></td>
+                </tr>
+                <tr>
+                    <td><b>Isu</b></td>
+                    <td>
+                        <div class="col-12">
+                        <textarea class="form-control" name="txtIsu" rows="10" required>{{ $minitCurai->isu }}</textarea>
+                        </div>
+                </tr>
+				<tr>
+						<td>Tanda Tangan Pegawai</td>
+						<td align= "center">Tarikh</td>
+				</tr>
+				<tr>
+						<td><br><br>
+						................................................
+						<br>
+						</td>
+						<td align="center"><br><br>
+						................................................
+						<br>
+						</td>
+				</tr>
+            </body>
+        </table>
+        
+    </form>
+</div>
